8e030116d44fa530bc2df5753db285ad2efb37b2
Jakub Semczyszyn <semcjaku@borg.kis.agh.edu.pl>
2018-03-06 08:33:11 +0100

    faktoryzacja factoriala

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -3,12 +3,16 @@
 //
 #include "Factorial.h"
 
-int factorial(int value) {
+bool checkPositiveRange(int value);
+
+int Factorial(int value) {
     if (value == 0 || value == 1)
         return 1;
-    if (value > 1 && value < 13)
-        return value * factorial(value - 1);
+    if (checkPositiveRange(value))
+        return value * Factorial(value - 1);
     if (value < 0)
-        return value * factorial(value + 1);
+        return value * Factorial(value + 1);
   return 0;
 }
+
+bool checkPositiveRange(int value) { return value > 1 && value < 13; }
