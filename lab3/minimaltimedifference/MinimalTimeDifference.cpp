f56b4592c853dc92aef8f2a57e24266b41c0acaf
Jakub Semczyszyn <semcjaku@borg.kis.agh.edu.pl>
2018-03-13 09:30:56 +0100

    nowsza wersja time

--- lab3/minimaltimedifference/MinimalTimeDifference.cpp
+++ lab3/minimaltimedifference/MinimalTimeDifference.cpp
@@ -6,9 +6,9 @@
 
 unsigned int ToMinutes(std::string time_HH_MM)
 {
-    std::unsigned int minutes=0;
-    std::int separator = time_HH_MM.find(":",);
-    std::size_t *pos=*time_HH_MM[separator];
+    unsigned int minutes=0;
+    int separator = time_HH_MM.find(":",);
+    size_t *pos=*time_HH_MM[separator];
     minutes+=(60*stoi(time_HH_MM,pos,10));
     std::string tmp = time_HH_MM.substr(separator+1);
     pos=*tmp;
@@ -18,5 +18,16 @@ unsigned int ToMinutes(std::string time_HH_MM)
 
 unsigned int MinimalTimeDifference(std::vector<std::string> times)
 {
-
+    unsigned int time1, time2;
+    time1=ToMinutes(times[0]);
+    time2=ToMinutes(times[1]);
+    if(abs(time1-time2)<=12*60)
+    {
+        time1=abs(time1-time2);
+    }
+    else
+    {
+        time1=24*60-abs(time1-time2);
+    }
+    return time1;
 }
