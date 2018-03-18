2fc68fb82bbdbe7c954494493331a3845ea98321
semcjaku <kubase.se@gmail.com>
2018-03-18 16:39:34 +0100

    lab3 zad2

--- lab3/minimaltimedifference/MinimalTimeDifference.cpp
+++ lab3/minimaltimedifference/MinimalTimeDifference.cpp
@@ -7,27 +7,33 @@
 unsigned int ToMinutes(std::string time_HH_MM)
 {
     unsigned int minutes=0;
-    int separator = time_HH_MM.find(":",);
-    size_t *pos=*time_HH_MM[separator];
-    minutes+=(60*stoi(time_HH_MM,pos,10));
-    std::string tmp = time_HH_MM.substr(separator+1);
-    pos=*tmp;
-    minutes+=stoi(time_HH_MM,pos,10);
+    size_t pos;
+    minutes+=(60*stoi(time_HH_MM,&pos,10));
+    std::string time_MM = time_HH_MM.substr(pos+1,std::string::npos);
+    minutes+=(stoi(time_MM,nullptr,10));
     return minutes;
 }
 
 unsigned int MinimalTimeDifference(std::vector<std::string> times)
 {
-    unsigned int time1, time2;
-    time1=ToMinutes(times[0]);
-    time2=ToMinutes(times[1]);
-    if(abs(time1-time2)<=12*60)
+   unsigned int tmp1, tmp2, minimal=1440;
+    for(int i=0; i<times.size()-1; i++)
     {
-        time1=abs(time1-time2);
+        tmp1=ToMinutes(times[i]);
+        for(int j=i+1; j<times.size(); j++)
+        {
+            tmp2=ToMinutes(times[j]);
+            if(abs(tmp1-tmp2)<=12*60)
+            {
+                tmp2=abs(tmp1-tmp2);
+            }
+            else
+            {
+                tmp2=24*60-abs(tmp1-tmp2);
+            }
+            if(tmp2<minimal)
+                minimal=tmp2;
+        }
     }
-    else
-    {
-        time1=24*60-abs(time1-time2);
-    }
-    return time1;
+    return minimal;
 }
