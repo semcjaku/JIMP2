ba8902a0f2005528ee4ff1c081d7b76d84263e9c
semcjaku <kubase.se@gmail.com>
2018-03-26 14:21:31 +0200

    template engine all tests passed

--- lab4/netstemplateengine/SimpleTemplateEngine.cpp
+++ lab4/netstemplateengine/SimpleTemplateEngine.cpp
@@ -20,7 +20,7 @@ namespace nets
 
     std::string View::Render(const std::unordered_map <std::string, std::string> &model) const
     {
-        std::size_t posbeg, posend;
+        std::size_t posbeg, posend, exception;
         std::string inserted, tmp;
         inserted.clear();
         posbeg=text_.length()+1;
@@ -33,6 +33,23 @@ namespace nets
             {
 
                 posend=text_.find("}}", posbeg+2);
+                exception=text_.find("}", posbeg);
+                if(exception<posend)                        //sprawdza przypadki typu "{{tekst} tekst {tekst}}"
+                {
+                    exception=posbeg;
+                    posbeg=text_.find("{{", posbeg+1);
+                    inserted+=text_.substr(exception, posbeg-(exception+1));
+                    posend=text_.find("}}", posbeg+2);
+                }
+                exception=text_.find("{{{");
+                if(exception==posbeg)
+                    inserted+=" ";
+                while(exception==posbeg)                    //sprawdza przypadki, gdy tekst obłożony więcej niż 2 nawiasami
+                {
+                    exception=text_.find("{{{");
+                    inserted+="{";
+                    posbeg++;
+                }
                 tmp=text_.substr(posbeg+2,posend-(posbeg+2));
                 auto map_check=model.find(tmp);
                 if(map_check!=model.end())
