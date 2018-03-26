da0f8f2b32c497014fcfbb224f59dee229c099c7
semcjaku <kubase.se@gmail.com>
2018-03-26 18:15:29 +0200

    lab4 zad6

--- lab4/netsjson/SimpleJson.cpp
+++ lab4/netsjson/SimpleJson.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "SimpleJson.h"
+
+namespace nets
+{
+    std::experimental::optional<JsonValue> JsonValue::ValueByName(const std::string &name) const
+    {
+        if(type==6)
+        {
+            auto map_check=m_value.find(name);
+            if(map_check!=m_value.end())
+                return map_check->second;
+        }
+    }
+
+    std::string JsonValue::ToString() const
+    {
+        std::string output;
+        switch (type)
+        {
+            case 1:
+                output=std::to_string(i_value);
+                break;
+            case 2:
+                output=std::to_string(d_value);
+                while(output[output.length()-1]=='0')
+                    output.erase(output.length()-1,1);
+                if(output[output.length()-1]=='.')
+                    output+="0";
+                break;
+            case 3:
+                if(b_value)
+                    output="true";
+                else
+                    output="false";
+                break;
+            case 4:
+                output="\""+s_value+"\"";
+                break;
+            case 5:
+                output="[";
+                for(auto n:v_value)
+                    output+=n.ToString()+", ";
+                output.erase(output.length()-2,2);
+                output+="]";
+                break;
+        }
+        return output;
+    }
+}
--- lab4/netsjson/SimpleJson.h
+++ lab4/netsjson/SimpleJson.h
@@ -7,18 +7,30 @@
 
 #include <experimental/optional>
 #include <string>
+#include <vector>
+#include <map>
 
 namespace nets
 {
     class JsonValue
     {
     public:
+        int i_value, type;
+        double d_value;
+        bool b_value;
+        std::string s_value;
+        std::vector<JsonValue> v_value;
+        std::map<std::string, JsonValue> m_value;
         JsonValue();
-        ~JsonValue();
+        JsonValue(int a) : i_value(a) {type=1;}
+        JsonValue(double a) : d_value(a) {type=2;}
+        JsonValue(bool a) : b_value(a) {type=3;}
+        JsonValue(std::string a) : s_value(a) {type=4;}
+        JsonValue(std::vector<JsonValue> a) : v_value(a) {type=5;}
+        JsonValue(std::map<std::string, JsonValue> a) : m_value(a) {type=6;}
 
         std::experimental::optional<JsonValue> ValueByName(const std::string &name) const;
         std::string ToString() const;
-    private:
 
     };
 }
