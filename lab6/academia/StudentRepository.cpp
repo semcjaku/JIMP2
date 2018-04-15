4e25c9d79980f28b76d34f409b545d408e12eb8c
Unknown <kubase.se@gmail.com>
2018-04-15 13:31:56 +0200

    lab6 zad2

--- lab6/academia/StudentRepository.cpp
+++ lab6/academia/StudentRepository.cpp
@@ -8,7 +8,7 @@ namespace academia
 {
     StudyYear::StudyYear()
     {
-        year=0;
+        year=1;
     }
 
     StudyYear::StudyYear(int y)
@@ -56,15 +56,51 @@ namespace academia
         return(year==y.year);
     }
 
-    bool StudyYear::operator==(const int &y) const
-    {
-        return (year==y);
-    }
-
     bool StudyYear::operator<(const StudyYear &y) const
     {
         return(year<y.year);
     }
+
+    StudyYear::operator int()
+    {
+        return year;
+    }
+
+    int StudyYear::GetYear() const
+    {
+        return year;
+    }
+
+    void StudyYear::SetYear(int y)
+    {
+        year=y;
+    }
+
+    bool operator==(const int &i, const StudyYear &sy)
+    {
+        return (i==sy.GetYear());
+    }
+
+    bool StudyYear::operator!=(const StudyYear &sy) const
+    {
+        return (year!=sy.year);
+    }
+
+    std::ostream& operator<< (std::ostream &out,StudyYear const &sy)
+    {
+        out<<sy.GetYear();
+        return out;
+    }
+
+    std::istream& operator>> (std::istream &in,StudyYear &sy)
+    {
+        int tmp;
+        in>>tmp;
+        sy.SetYear(tmp);
+        return in;
+    }
+
+ //=======================================================================================================
 
     Student::Student(string i, string fn, string ln, string p, StudyYear y)
     {
@@ -75,4 +111,166 @@ namespace academia
         year=y;
     }
 
+    bool Student::operator==(const Student &s) const
+    {
+        return (id==s.id && first_name==s.first_name && last_name==s.last_name && program==s.program && year==s.year);
+    }
+
+    bool Student::operator!=(const Student &s) const
+    {
+        return (id!=s.id || first_name!=s.first_name || last_name!=s.last_name || program!=s.program || year!=s.year);
+    }
+
+    string Student::Id()
+    {
+        return id;
+    }
+
+    string Student::FirstName()
+    {
+        return first_name;
+    }
+
+    string Student::LastName()
+    {
+        return last_name;
+    }
+
+    string Student::Program()
+    {
+        return program;
+    }
+
+    StudyYear Student::Year()
+    {
+        return year;
+    }
+
+    void Student::ChangeLastName(string new_ln)
+    {
+        last_name=new_ln;
+    }
+    void Student::ChangeId(string new_id)
+    {
+        id=new_id;
+    }
+
+    void Student::ChangeFirstName(string new_fn)
+    {
+        first_name=new_fn;
+    }
+
+    void Student::ChangeProgram(string new_p)
+    {
+        program=new_p;
+    }
+
+    void Student::ChangeYear(int y)
+    {
+        year.SetYear(y);
+    }
+
+    std::ostream& operator<< (std::ostream &out,Student &s)
+    {
+        out<<"Student {id: "<<s.Id()<<", first_name: "<<s.FirstName()<<", last_name: "<<s.LastName()<<", program: "<<s.Program()<<", year:"<<s.Year()<<"}";
+        return out;
+    }
+
+    std::istream& operator>> (std::istream &in,Student &s)
+    {
+        string tmp;
+        size_t p=0,k=0;
+        in>>tmp;
+        p=tmp.find('"', 0);
+        k=tmp.find('"', p);
+        s.ChangeId(tmp.substr(p+1,k-p-1));
+        p=tmp.find('"', k);
+        k=tmp.find('"', p);
+        s.ChangeFirstName(tmp.substr(p+1,k-p-1));
+        p=tmp.find('"', k);
+        k=tmp.find('"', p);
+        s.ChangeLastName(tmp.substr(p+1,k-p-1));
+        p=tmp.find('"', k);
+        k=tmp.find('"', p);
+        s.ChangeProgram(tmp.substr(p+1,k-p-1));
+        p=tmp.find(": ",k);
+        k=tmp.find('}',p);
+        s.ChangeYear(std::stoi(tmp.substr(p+1,k-p-1),nullptr,10));
+        return in;
+    }
+
+    //=======================================================================================================
+
+    StudentRepository::StudentRepository()
+    {
+        students.clear();
+    }
+
+    bool StudentRepository::operator==(const StudentRepository &sr) const
+    {
+        bool isin;
+        size_t count=0;
+        for(Student n:students)
+        {
+            isin=false;
+            for(Student m:sr.students)
+            {
+                if(n==m)
+                    isin=true;
+            }
+            if(isin)
+                count++;
+        }
+        if(count==students.size())
+            return true;
+        return false;
+    }
+
+    Student & StudentRepository::operator[](string id)
+    {
+        for(Student &n:students)
+        {
+            if(n.Id()==id)
+                return n;
+        }
+    }
+
+    const Student & StudentRepository::operator[](string id) const
+    {
+        for(Student n:students)
+        {
+            if(n.Id()==id)
+                return n;
+        }
+    }
+
+    int StudentRepository::StudentCount()
+    {
+        return (int)students.size();
+    }
+
+    std::vector<Student> StudentRepository::Students()
+    {
+        return students;
+    }
+
+    std::ostream& operator<< (std::ostream &out,StudentRepository &sr)
+    {
+        out<<'[';
+        for(Student n:sr.Students())
+        {
+            out<<n;
+            if (n!=sr.Students()[sr.Students().size()-1])
+                out<<", ";
+        }
+        out<<']';
+        return out;
+    }
+
+    std::istream& operator>> (std::istream &in,StudentRepository &sr)
+    {
+        for(Student &n:sr.Students())
+            in>>n;
+        return in;
+    }
 }
--- lab6/academia/StudentRepository.h
+++ lab6/academia/StudentRepository.h
@@ -7,6 +7,9 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
+#include <initializer_list>
+
 using std::string;
 
 namespace academia
@@ -23,15 +26,23 @@ namespace academia
         StudyYear operator--();
         StudyYear operator--(int);
         bool operator==(const StudyYear &y) const;
-        bool operator==(const int &y) const;
+        bool operator!=(const StudyYear &sy) const;
         bool operator<(const StudyYear &y) const;
+        operator int();
 
         StudyYear(const StudyYear& sy);
         StudyYear & operator=(const StudyYear& sy);
 
+        int GetYear() const;
+        void SetYear(int y);
+
     private:
         int year;
     };
+
+    bool operator==(const int &i, const StudyYear &sy);
+    std::ostream& operator<< (std::ostream&,StudyYear const&);
+    std::istream& operator>> (std::istream&,StudyYear&);
 
     class Student
     {
@@ -39,6 +50,21 @@ namespace academia
         Student()=default;
         Student(string i, string fn, string ln, string p, StudyYear y);
         ~Student()=default;
+
+        bool operator==(const Student &s) const;
+        bool operator!=(const Student &s) const;
+
+        string Id();
+        string FirstName();
+        string LastName();
+        string Program();
+        StudyYear Year();
+        void ChangeLastName(string new_ln);
+        void ChangeId(string new_id);
+        void ChangeFirstName(string new_fn);
+        void ChangeProgram(string new_p);
+        void ChangeYear(int y);
+
     private:
         string id;
         string first_name;
@@ -48,13 +74,29 @@ namespace academia
 
     };
 
+    std::ostream& operator<< (std::ostream&,Student&);
+    std::istream& operator>> (std::istream&,Student&);
+
     class StudentRepository
     {
     public:
+        StudentRepository();
+        StudentRepository(std::initializer_list<Student> list) : students{list} {}
+        ~StudentRepository()=default;
+
+        bool operator==(const StudentRepository &sr) const;
+        Student & operator[](string id);
+        const Student & operator[](string id) const;
+
+        int StudentCount();
+        std::vector<Student> Students();
 
     private:
         std::vector<Student> students;
     };
+
+    std::ostream& operator<< (std::ostream&,StudentRepository&);
+    std::istream& operator>> (std::istream&,StudentRepository&);
 }
 
 #endif //JIMP_EXERCISES_STUDENTREPOSITORY_H
